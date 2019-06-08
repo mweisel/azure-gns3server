f8bbb5d510fc75f5228d3780da10d5b57ec7080d
Marc Weisel <mweisel@users.noreply.github.com>
2019-06-07 17:14:17 -0700

    syntax changes for terraform v0.12

--- main.tf
+++ main.tf
@@ -2,34 +2,34 @@ provider "azurerm" {}
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-resources"
-  location = "${var.location}"
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
-  address_space       = ["${var.address_space}"]
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  address_space       = var.address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "${var.prefix}-internal_subnet"
-  resource_group_name  = "${azurerm_resource_group.rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  address_prefix       = "${var.address_prefix}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = var.address_prefix
 }
 
 resource "azurerm_public_ip" "public_ip" {
   name                = "${var.prefix}-public_ip"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_security_group" "allow-ssh-and-openvpn" {
   name                = "${var.prefix}-nsg_ssh_openvpn"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
     name                       = "SSH"
@@ -58,25 +58,25 @@ resource "azurerm_network_security_group" "allow-ssh-and-openvpn" {
 
 resource "azurerm_network_interface" "nic" {
   name                = "${var.prefix}-nic"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "ipconfig"
-    subnet_id                     = "${azurerm_subnet.internal.id}"
+    subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.public_ip.id}"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
 resource "azurerm_virtual_machine" "vm" {
   name                  = "${var.prefix}-server"
-  location              = "${azurerm_resource_group.rg.location}"
-  resource_group_name   = "${azurerm_resource_group.rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
-  vm_size               = "${var.vm_size}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = var.vm_size
 
-  delete_os_disk_on_termination = "${var.delete_os_disk}"
+  delete_os_disk_on_termination = var.delete_os_disk
 
   storage_image_reference {
     publisher = "Canonical"
@@ -89,21 +89,21 @@ resource "azurerm_virtual_machine" "vm" {
     name              = "${var.prefix}-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "${var.disk_type}"
-    disk_size_gb      = "${var.disk_size}"
+    managed_disk_type = var.disk_type
+    disk_size_gb      = var.disk_size
   }
 
   os_profile {
-    computer_name  = "${var.computer_name}"
-    admin_username = "${var.admin_username}"
+    computer_name  = var.computer_name
+    admin_username = var.admin_username
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
 
-    ssh_keys = [{
-      key_data = "${file("~/.ssh/id_rsa.pub")}"
+    ssh_keys {
+      key_data = file("~/.ssh/id_rsa.pub")
       path     = "/home/${var.admin_username}/.ssh/authorized_keys"
-    }]
+    }
   }
 }
